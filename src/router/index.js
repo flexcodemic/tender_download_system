ba4606b7fdeff398e8cff47976bc5ec16096b92f
Anthony Aruma <anthonyaruma091@gmail.com>
2025-02-20 14:37:26 +0100

    create and define the Router folder and files

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from '@/views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginView,
+    },
+    {
+        path: '/: pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFoundView,
+    },
+
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
