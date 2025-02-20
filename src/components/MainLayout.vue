3b30d4c62b6c19b9866e3ceea6ed4a55d84a1cc3
Anthony Aruma <anthonyaruma091@gmail.com>
2025-02-20 15:54:58 +0100

    Add logout button to the mainlayout page

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -2,10 +2,35 @@
   <div>
     <nav class="navbar navbar-dark bg-dark px-3">
       <router-link to="/" class="navbar-brand">Tender System</router-link>
-      <router-link to="/login" class="btn btn-outline-light">Login</router-link>
+      <div>
+        <button v-if="!isLoggedIn" @click="goToLogin" class="btn btn-outline-light">Login</button>
+        <button v-if="isLoggedIn" @click="logout" class="btn btn-danger">Logout</button>
+      </div>
     </nav>
     <div class="container mt-4">
-      <router-view></router-view> <!-- loads the current page -->
+      <router-view></router-view>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const isLoggedIn = ref(false);
+
+onMounted(() => {
+  isLoggedIn.value = !!localStorage.getItem('user');
+});
+
+const goToLogin = () => {
+  router.push('/login');
+};
+
+const logout = () => {
+  localStorage.removeItem('user');
+  isLoggedIn.value = false;
+  router.push('/login');
+};
+</script>
